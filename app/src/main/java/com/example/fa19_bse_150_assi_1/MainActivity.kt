a484f030e62a31f3467372a0fc3b87e3348f1f84
SHAMSHER_KHAN <123456>
2022-04-22 14:57:28 +0500

    0

---
+++ app/src/main/java/com/example/fa19_bse_150_assi_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fa19_bse_150_assi_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
